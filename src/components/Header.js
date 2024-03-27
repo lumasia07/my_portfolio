9b0a14285dbc87d60690ec31b11c81e435c44c35
lumasia07 <lumasiastancelaus@gmail.com>
2024-03-27 13:16:19 +0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='intro'>
+      <h1>Hello, I am Lumasia, a Fullstack<br>
+      </br>Software Engineer</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Menubar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Menubar.css'
+
+class Menubar extends Component {
+  render() {
+    return (
+      <div className='menubar'>
+        <ul>
+            <li>My Skills</li>
+            <li>Bio</li>
+            <li>My Services</li>
+            <li>Contact Me</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Menubar
