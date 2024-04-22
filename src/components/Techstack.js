5c656ab0ea4a884558f30732368bf8c11d1ac74b
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-22 19:42:11 +0300

    techstack

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react'
 import './Techstack.css'
 
-export class Techstack extends Component {
-  render() {
-    return (
-      <div className='techstack'>
-        <h2>Technical skills</h2>
-        <ul>
-          <li id='html5'>HTML 5</li>
-          <li>React JS</li>
-          <li>Node JS</li>
-          <li>Tailwind CSS</li>
-          <li>Next JS</li>
-          <li>MySQL</li>
-          <li>Linux</li>
-          <li>Git & Github</li>
-          <li>Python</li>
-          <li>Bash Scripting</li>
-          <li>C</li>
-        </ul>
-      </div>
-    )
-  }
+const skills = [
+  'HTML5',
+  'React JS',
+  'Node JS',
+  'Next JS',
+  'Tailwind CSS',
+  'Linux'
+];
+
+const Techstack = () => {
+  return (
+    <div className='techstack'>
+      <h2>My Skills</h2>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Techstack
