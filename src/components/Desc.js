ba7c2c40e6e3d0e0f0e9ed0f35c766a08144afe3
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-22 21:09:40 +0300

    desc

---
+++ src/components/Desc.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Techstack from './Techstack'
+import './Desc.css'
+
+class Desc extends Component {
+  render() {
+    return (
+      <div className='desc'>
+        <p>
+        Lumasia is a frontend engineer with a background in Computer Science.
+        He excels in creating user-friendly interfaces using technologies like HTML, CSS, and JavaScript frameworks. 
+        With strong communication skills, he collaborates effectively with teams and prioritizes accessibility in his work.
+        Passionate about user-centric design, he actively contributes to making the web a more inclusive space.
+        </p>
+      </div>
+    )
+  }
+
+}
+
+export default Desc
